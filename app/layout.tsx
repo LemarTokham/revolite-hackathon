a784217056ffe468d7e69c1cfebd42c833049ee1
LemarTokham <ltokham@gmail.com>
2026-03-21 14:16:35 +0000

    change page name

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "FakeBank",
+  title: "Revolite",
 };
 
 export default function RootLayout({
